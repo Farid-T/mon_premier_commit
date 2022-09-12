d4f1c14a49e79358efda28f1ffffb17e29592750
Farid-T <startmob@gmail.com>
2022-09-12 16:04:47 +0200

    mon_prmier_commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let scorepsg = 12;
+let scoreRealMadrid = 212;
+
+if (scorepsg > scoreRealMadrid){
+console.log("vous êtes en train de réver il est temps d'aller au boulot")
+}else if (scorepsg < scoreRealMadrid) {
+console.log("bah logique!")
+}else {
+console.log("Quoi ! match nulle")
+}
